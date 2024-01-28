9aa74e1a370c813a25da226475919063e45b961b
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-01-29 02:46:22 +0900

    under construction

--- src/ros_blink1_driver.cpp
+++ src/ros_blink1_driver.cpp
@@ -1,14 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/color_rgba.hpp"
 
-void topicCallback(const std_msgs::msg::String::SharedPtr msg) {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "I heard: '%s'", msg->data.c_str());
+void topicCallback(const std_msgs::msg::ColorRGBA::SharedPtr msg) {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "I heard: '%f %f %f'", msg->r,  msg->g, msg->b);
 }
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("subscriber_node");
-    auto subscription = node->create_subscription<std_msgs::msg::String>("topic", 10, topicCallback);
+    auto node = rclcpp::Node::make_shared("ros_blink1_driver_node");
+    auto subscription = node->create_subscription<std_msgs::msg::ColorRGBA>("blink1_color", 10, topicCallback);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
